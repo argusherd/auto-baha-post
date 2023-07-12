9fae194d1993a146314f70fc6ed193a5fa9b4e88
arguewithsheep <a7752876@gmail.com>
2023-07-12 13:57:10 +0800

    add Draft entity

---
+++ electron-src/database/entities/Draft.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Draft extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  subject: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
