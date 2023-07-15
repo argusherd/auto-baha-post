fadc4f8d68405970ca0e877520d9fcd760bf1504
arguewithsheep <a7752876@gmail.com>
2023-07-15 14:05:15 +0800

    install express, cors, @types

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,7 @@
+import { configDotenv } from "dotenv";
 import nextJest from "next/jest.js";
+
+configDotenv({ path: ".env.test" });
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
