a91d1888ab724bdbae98515f73cd849961478316
arguewithsheep <a7752876@gmail.com>
2023-08-25 17:53:42 +0800

    a board cannot be deleted if a post is assign to it

--- __tests__/feature/boards/destroy.test.ts
+++ __tests__/feature/boards/destroy.test.ts
@@ -1,6 +1,7 @@
 import app from "@/backend-api";
 import Board from "@/backend-api/database/entities/Board";
 import BoardFactory from "@/backend-api/database/factories/BoardFactory";
+import PostFactory from "@/backend-api/database/factories/PostFactory";
 import supertest from "supertest";
 
 describe("delete a board api", () => {
@@ -18,5 +19,16 @@ describe("delete a board api", () => {
     const notExists = 99999;
 
     await supertest(app).delete(`/api/boards/${notExists}`).expect(404);
+  });
+
+  it("cannot delete a board that is been assigned to a post", async () => {
+    const board = await new BoardFactory().create();
+    await new PostFactory().create({ board });
+
+    await supertest(app).delete(`/api/boards/${board.id}`).expect(409);
+
+    const stillExist = await Board.findOneBy({ id: board.id });
+
+    expect(stillExist).not.toBeNull();
   });
 });
