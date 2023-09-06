e68de73b3b0c706b3f5f605e15a89d890b7b628f
arguewithsheep <a7752876@gmail.com>
2023-09-06 19:51:16 +0800

    a board cannot be deleted if it's assigned to a post ready to published

--- __tests__/feature/boards/destroy.test.ts
+++ __tests__/feature/boards/destroy.test.ts
@@ -2,6 +2,7 @@ import app from "@/backend-api";
 import Board from "@/backend-api/database/entities/Board";
 import BoardFactory from "@/backend-api/database/factories/BoardFactory";
 import PostFactory from "@/backend-api/database/factories/PostFactory";
+import moment from "moment";
 import supertest from "supertest";
 
 describe("delete a board api", () => {
@@ -21,14 +22,27 @@ describe("delete a board api", () => {
     await supertest(app).delete(`/api/boards/${notExists}`).expect(404);
   });
 
-  it("cannot delete a board that is been assigned to a post", async () => {
+  it("cannot delete a board that has been assigned to a post that is ready to be published", async () => {
+    jest.useFakeTimers();
+
     const board = await new BoardFactory().create();
-    await new PostFactory().create({ board });
+    await new PostFactory().create({
+      board,
+      scheduled_at: moment().add(1, "hour").toISOString(),
+    });
 
     await supertest(app).delete(`/api/boards/${board.id}`).expect(409);
 
     const stillExist = await Board.findOneBy({ id: board.id });
 
     expect(stillExist).not.toBeNull();
+
+    jest.setSystemTime(moment().add(2, "hours").toDate());
+
+    await supertest(app).delete(`/api/boards/${board.id}`).expect(200);
+
+    const deleted = await Board.findOneBy({ id: board.id });
+
+    expect(deleted).toBeNull();
   });
 });
