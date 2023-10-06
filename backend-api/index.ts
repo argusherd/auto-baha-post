8e827667277a46463daccd410b08be1f6df6b2f1
arguewithsheep <a7752876@gmail.com>
2023-10-06 17:35:41 +0800

    check login status api

--- backend-api/index.ts
+++ backend-api/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import boards from "./controllers/boards";
+import login from "./controllers/login";
 import posts from "./controllers/posts";
 
 const app = express();
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use("/api", posts);
 app.use("/api", boards);
+app.use("/api", login);
 
 export default app;
