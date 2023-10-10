2ec8ef914e45671803e0ae30056e96cb7d4520eb
arguewithsheep <a7752876@gmail.com>
2023-10-10 19:18:17 +0800

    display login status

--- renderer/app/header.tsx
+++ renderer/app/header.tsx
@@ -1,9 +1,39 @@
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import Login from "../../backend-api/database/entities/Login";
 
 export default function Header() {
+  const [userInfo, setUserInfo] = useState<Partial<Login>>({
+    name: null,
+    account: null,
+    logged_in: false,
+    created_at: null,
+  });
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const res = await axios.get(`${window.backendUrl}/api/login/check`);
+
+      setUserInfo(res?.data);
+    };
+
+    checkLogin();
+  }, []);
+
   return (
     <header>
       <Link href={"/"}>Home</Link>
+      {userInfo?.name ? (
+        <span
+          data-testid="userinfo"
+          title={`Last time checked at: ${new Date(userInfo.created_at)}`}
+        >
+          {`${userInfo.name} (${userInfo.account})`}
+        </span>
+      ) : (
+        <span data-testid="userinfo">User is not logged in yet</span>
+      )}
       <button onClick={() => window.electron.openBaha()}>Open Baha</button>
     </header>
   );
